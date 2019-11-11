81f998638e6c72b504fee1b4b2af1e38190b974c
majorova-vs <valerya929@gmail.com[core]>
2019-11-12 00:35:28 +0300

    Добавьте файлы проекта.

---
+++ Datamatrix/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Datamatrix
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEncode = new System.Windows.Forms.Button();
+            this.labelEncode = new System.Windows.Forms.Label();
+            this.textBoxEnc = new System.Windows.Forms.TextBox();
+            this.picBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnEncode
+            // 
+            this.btnEncode.Location = new System.Drawing.Point(410, 327);
+            this.btnEncode.Name = "btnEncode";
+            this.btnEncode.Size = new System.Drawing.Size(75, 23);
+            this.btnEncode.TabIndex = 0;
+            this.btnEncode.Text = "Encode";
+            this.btnEncode.UseVisualStyleBackColor = true;
+            this.btnEncode.Click += new System.EventHandler(this.btnEncode_Click);
+            // 
+            // labelEncode
+            // 
+            this.labelEncode.AutoSize = true;
+            this.labelEncode.Location = new System.Drawing.Point(12, 328);
+            this.labelEncode.Name = "labelEncode";
+            this.labelEncode.Size = new System.Drawing.Size(35, 17);
+            this.labelEncode.TabIndex = 1;
+            this.labelEncode.Text = "Text";
+            // 
+            // textBoxEnc
+            // 
+            this.textBoxEnc.Location = new System.Drawing.Point(53, 328);
+            this.textBoxEnc.Name = "textBoxEnc";
+            this.textBoxEnc.Size = new System.Drawing.Size(333, 22);
+            this.textBoxEnc.TabIndex = 2;
+            // 
+            // picBox
+            // 
+            this.picBox.BackColor = System.Drawing.Color.White;
+            this.picBox.Location = new System.Drawing.Point(25, 12);
+            this.picBox.Name = "picBox";
+            this.picBox.Size = new System.Drawing.Size(492, 297);
+            this.picBox.TabIndex = 3;
+            this.picBox.TabStop = false;
+            this.picBox.Click += new System.EventHandler(this.picBox_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(538, 374);
+            this.Controls.Add(this.picBox);
+            this.Controls.Add(this.textBoxEnc);
+            this.Controls.Add(this.labelEncode);
+            this.Controls.Add(this.btnEncode);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DataMatrix Generator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEncode;
+        private System.Windows.Forms.Label labelEncode;
+        private System.Windows.Forms.TextBox textBoxEnc;
+        private System.Windows.Forms.PictureBox picBox;
+    }
+}
+
---
+++ Datamatrix/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DataMatrix.net;
+
+namespace Datamatrix
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEncode_Click(object sender, EventArgs e)
+        {
+            DmtxImageEncoder encoder = new DmtxImageEncoder();
+            Bitmap img = encoder.EncodeImage(textBoxEnc.Text);
+            
+            //img.Size = picBox.Size;
+            picBox.Image = img;
+            picBox.Width = img.Width;
+            picBox.Height = img.Height;
+            picBox.Left = 139;
+            picBox.Top = 95;
+            
+        }
+
+        private void picBox_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
